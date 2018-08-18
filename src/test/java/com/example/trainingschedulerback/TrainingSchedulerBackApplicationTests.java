1d400cb8099f3db37a35775fb602e5ef84021062
Sławomir Pluciński <slawomir.plucinski@gmail.com>
2018-08-18 09:40:36 +0200

    init commit

---
+++ src/test/java/com/example/trainingschedulerback/TrainingSchedulerBackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.trainingschedulerback;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrainingSchedulerBackApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
